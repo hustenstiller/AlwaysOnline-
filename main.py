6897203c1f582e17df81c687b8467eee4ddb6c5e
hustenstiller <janina.75385@gmx.de>
2025-10-16 21:12:50 +0000

    fix

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import asyncio
 from login import client
 from telethon.tl.functions.account import UpdateStatusRequest
-import time
 from data import delay
 
-if client.is_user_authorized():
-    logging.info("You are now AlwaysOnline™, Yah!")
-    while True:
-        client(UpdateStatusRequest(offline=False))
-        time.sleep(delay)
-        logging.debug("Sleep for 1 min")
-else:
-    logging.fatal("Login Fails, please retry... 失败，请重试！")
+async def main():
+    if await client.is_user_authorized():
+        logging.info("You are now AlwaysOnline™, Yah!")
+        while True:
+            await client(UpdateStatusRequest(offline=False))
+            await asyncio.sleep(delay)
+            logging.debug("Sleep for %s seconds", delay)
+    else:
+        logging.fatal("Login Fails, please retry... 失败，请重试！")
+
+if __name__ == "__main__":
+    asyncio.run(main())
